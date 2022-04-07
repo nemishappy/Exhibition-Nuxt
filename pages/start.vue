b3b4b2e556d1d267e83f220bde68ac5a637aaa3c
nemishappy <neomk24@gmail.com>
2022-04-07 16:15:26 +0700

    init start page

--- pages/start.vue
+++ pages/start.vue
@@ -1,51 +1,58 @@
 <template>
   <div>
     <!-- gets re-positioned in myInit(); -->
+    <canvas id="myCanvas" ref="myCanvas"></canvas>
     <center>
-    <canvas id="myCanvas"></canvas>
       <img
-        src="~/assets/images/admin-region-02_fw.png"
+        src="~/assets/images/Thailand_Map.png"
         usemap="#imgmap_css_container_imgmap201293016112"
         class="imgmap_css_container"
-        title="imgmap201293016112"
         alt="imgmap201293016112"
         id="img-imgmap201293016112"
+        ref="img-imgmap201293016112"
+        width="701"
+        height="1280"
+        @load="myInit"
       />
 
       <map name="imgmap_css_container_imgmap201293016112">
         <area
           target=""
-          href=""
+          href="/inspire"
           @mouseover="myHover($event.currentTarget)"
           @mouseout="myLeave()"
           alt="imgmap201293016112-0"
-          title="imgmap201293016112-0"
+          title="ภาค 7"
           class="imgmap201293016112-area"
           id="imgmap201293016112-area-0"
-          coords="109,280,105,257,91,240,112,239,114,212,128,174,171,172,203,158,192,185,199,222,179,259,187,282,164,303,154,284,151,308"
+          coords="33,241,17,197,0,169,35,172,31,137,38,100,57,64,94,70,125,61,139,66,147,33,182,29,177,61,164,75,169,97,168,118,174,134,170,173,152,160,134,177,116,183,124,213,129,242,100,237,104,283,90,288,78,264,66,257,52,234"
           shape="poly"
         />
       </map>
-      
     </center>
   </div>
 </template>
 
 <script>
 var hdc
+var clipImage
+var img
 export default {
-  mounted() {
-    this.myInit()
+  data() {
+    return {
+      sector: [{}, {}],
+    }
   },
   methods: {
-    byId(e) {
-      return document.getElementById(e)
+    byRefs(e) {
+      return this.$refs[e]
     },
     drawPoly(coOrdStr) {
       var mCoords = coOrdStr.split(',')
       var i, n
       n = mCoords.length
 
+      hdc.save()
       hdc.beginPath()
       hdc.moveTo(mCoords[0], mCoords[1])
       for (i = 2; i < n; i += 2) {
@@ -53,50 +60,41 @@ export default {
       }
       hdc.lineTo(mCoords[0], mCoords[1])
       hdc.stroke()
-    },
-    drawRect(coOrdStr) {
-      var mCoords = coOrdStr.split(',')
-      var top, left, bot, right
-      left = mCoords[0]
-      top = mCoords[1]
-      right = mCoords[2]
-      bot = mCoords[3]
-      hdc.strokeRect(left, top, right - left, bot - top)
+      // make the current path a clipping path
+      hdc.clip()
+
+      // draw the image which will be clipped except in the clipping path
+      hdc.drawImage(clipImage, 0, 0)
+      img.style.opacity = 0.3
+
+      // restore the unclipped context (==undo the clipping path)
+      hdc.restore()
     },
     myHover(element) {
-      var hoveredElement = element
       var coordStr = element.getAttribute('coords')
-      var areaType = element.getAttribute('shape')
 
-      switch (areaType) {
-        case 'polygon':
-        case 'poly':
-          this.drawPoly(coordStr)
-          break
-
-        case 'rect':
-          this.drawRect(coordStr)
-      }
+      this.drawPoly(coordStr)
     },
     myLeave() {
-      var canvas = this.byId('myCanvas')
+      var canvas = this.byRefs('myCanvas')
       hdc.clearRect(0, 0, canvas.width, canvas.height)
+      img.style.opacity = 1.0
     },
     myInit() {
       // get the target image
-      var img = this.byId('img-imgmap201293016112')
+      img = this.byRefs('img-imgmap201293016112')
 
       var x, y, w, h
 
       // get it's position and width+height
-      x = img.offsetLeft
-      y = img.offsetTop
-      w = img.clientWidth
-      h = img.clientHeight
 
+      x = img.offsetLeft - 5
+      y = img.offsetTop - 5
+      w = img.clientWidth + 10
+      h = img.clientHeight + 10
       // move the canvas, so it's contained by the same parent as the image
       var imgParent = img.parentNode
-      var can = this.byId('myCanvas')
+      var can = this.byRefs('myCanvas')
       imgParent.appendChild(can)
 
       // place the canvas in front of the image
@@ -114,9 +112,16 @@ export default {
       hdc = can.getContext('2d')
 
       // set the 'default' values for the colour/width of fill/stroke operations
-      hdc.fillStyle = 'red'
-      hdc.strokeStyle = 'red'
-      hdc.lineWidth = 2
+      hdc.lineWidth = 1
+      hdc.shadowColor = 'black'
+      hdc.shadowBlur = 6
+      hdc.shadowOffsetX = 6
+      hdc.shadowOffsetY = 6
+      this.loadClipImage()
+    },
+    loadClipImage() {
+      clipImage = new Image()
+      clipImage.src = require('~/assets/images/Thailand_Map.png')
     },
   },
 }
